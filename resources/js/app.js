e2cef555b591b62791f164714695531d146b01aa
jSandersWhite <j.sanders.white@gmail.com>
2019-11-26 20:09:01 -0500

    Header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,3 @@
-require('./bootstrap');
+require('../../node_modules/mdbootstrap/js/mdb.js');
+require('../../node_modules/mdbootstrap/js/jquery.js')
+require('../../node_modules/mdbootstrap/js/bootstrap.js');
---
+++ resources/views/layouts/partials/nav.blade.php
@@ -0,0 +1,32 @@
+<header class="black">
+    <ul class="nav justify-content-end black px-4 .sticky-top">
+        <a href="http://mp.gg/umubn" class="text-uppercase btn btn-outline-green btn-sm">Join the Riot</a>
+    </ul>
+    <div class="justify-content-center black text-center">
+        <a class="mx-auto" href="{{ route('home') }}">
+            <img src="{{ asset('images/riot.png') }}" class="header-img">
+        </a>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark black text-white justify-content-center text-center">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="d-block collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Chants</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
